540aa68da12992b5a569638c5c96d7aa6b0a044a
papercrane1001 <papercrane1001@gmail.com>
2021-07-21 14:32:09 -0700

    It actually works now, and I added to the description and the preview image.

--- Source/ExpandedShells/ExpandedShells/ExpandedShells.cs
+++ Source/ExpandedShells/ExpandedShells/ExpandedShells.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using RimWorld;
+using UnityEngine;
 using Verse;
 using Verse.AI;
 using Verse.Sound;
@@ -12,32 +13,69 @@ using Verse.Sound;
 
 namespace ExpandedShells
 {
-    [DefOf]
-    public static class DamageDefOf
-    {
-        public static DamageDef SoapBomb;
-    }
-    public class ExpandedShells
+    //[DefOf]
+    //public static class DamageDefOf
+    //{
+    //    public static DamageDef SoapBomb;
+    //}
+    //public class ExpandedShells
+    //{
+
+    //}
+
+    public class Projectile_SoapBomb : Projectile_Explosive
     {
 
-    }
-
-    public class ExplosionEffect_Clean : CompProperties_Explosive
-    {
-        //RimWorld.CompExplosive
-        //RimWorld.DamageDefOf
-
-        public ExplosionEffect_Clean() : base()
+        protected override void Explode()
         {
-            explosiveDamageType = DamageDefOf.SoapBomb;
+            
+            //base.Explode();
+            IntVec3 target = this.Position;
+            Map map = this.Map;
+
+            float range = 16.9F;
+
+            for(int i = target.x - (int)range; i < target.x + (int)range; ++i)
+            {
+                for(int j = target.z - (int)range; j < target.z + (int) range; ++j)
+                {
+                    if(Math.Pow(Math.Pow(target.x - i, 2) + Math.Pow(target.z - j, 2), 0.5) < range)
+                    {
+                        IntVec3 ivt = new IntVec3(i, target.y, j);
+                        List<Thing> thingList = ivt.GetThingList(map);
+                        for(int k = 0; k < thingList.Count; ++k)
+                        {
+                            Filth filth = thingList[k] as Filth;
+                            if(filth != null)
+                            {
+                                filth.Destroy();
+                                SpawnFleck(new LocalTargetInfo(ivt), FleckDefOf.Smoke, map);
+                            }
+                        }
+                    }
+                }
+            }
+            base.Explode();
         }
 
-        //public void Apply(LocalTargetInfo target, LocalTargetInfo dest)
-        //{
-            
-        //}
+        protected override void Impact(Thing hitThing)
+        {
+            base.Impact(hitThing);
+            Explode();
+            return;
+        }
 
-        //public override 
+        protected void SpawnFleck(LocalTargetInfo target, FleckDef def, Map map)
+        {
+            if (target.HasThing)
+            {
+                FleckMaker.AttachedOverlay(target.Thing, def, Vector3.zero, 1f);
+            }
+            else
+            {
+                FleckMaker.Static(target.Cell, map, def, 1f);
+            }
+        }
     }
 
 
