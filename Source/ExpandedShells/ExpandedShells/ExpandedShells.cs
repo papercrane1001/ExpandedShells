fa7e7852e0e257bf77bf0172d98f2b571b554bbf
papercrane1001 <papercrane1001@gmail.com>
2021-07-22 09:44:01 -0700

    Cleaned, created file for boomrat shell

--- Source/ExpandedShells/ExpandedShells/ExpandedShells.cs
+++ Source/ExpandedShells/ExpandedShells/ExpandedShells.cs
@@ -13,23 +13,10 @@ using Verse.Sound;
 
 namespace ExpandedShells
 {
-    //[DefOf]
-    //public static class DamageDefOf
-    //{
-    //    public static DamageDef SoapBomb;
-    //}
-    //public class ExpandedShells
-    //{
-
-    //}
-
     public class Projectile_SoapBomb : Projectile_Explosive
     {
-
         protected override void Explode()
         {
-            
-            //base.Explode();
             IntVec3 target = this.Position;
             Map map = this.Map;
 
@@ -77,28 +64,4 @@ namespace ExpandedShells
             }
         }
     }
-
-
-    //public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
-    //{
-    //    base.Apply(target, dest);
-    //    Map map = parent.pawn.Map;
-    //    foreach (IntVec3 item in Utilities.AffectedCells(target, map, parent))
-    //    {
-    //        List<Thing> thingList = item.GetThingList(map);
-    //        for (int i = 0; i < thingList.Count; i++)
-    //        {
-    //            Filth filth = thingList[i] as Filth;
-    //            if (filth != null)
-    //            {
-    //                filth.Destroy();
-    //                SoundDefOf.Psycast_Skip_Exit.PlayOneShot(new TargetInfo(item, map));
-    //                Utilities.SpawnFleck(new LocalTargetInfo(item), FleckDefOf.PsycastSkipInnerExit, map);
-    //                Utilities.SpawnFleck(new LocalTargetInfo(item), FleckDefOf.PsycastSkipOuterRingExit, map);
-    //                Utilities.SpawnEffecter(new LocalTargetInfo(item), EffecterDefOf.Skip_Exit, map, 60, parent);
-    //                //Mote mote = MoteMaker.MakeStaticMote(item.ToVector3Shifted(), map, ThingDefOf.Mote_WaterskipSplashParticles);
-    //            }
-    //        }
-    //    }
-    //}
 }
